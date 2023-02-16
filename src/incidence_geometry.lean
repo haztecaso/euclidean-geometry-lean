13a68fd8f6e6112c4f98c9d3fd3e38a332043ab4
Adrián Lattes <adrianlattes@disroot.org>
2023-02-16 08:12:54 +0000

    Notación para lies_on y empezando prueba primera prop

--- src/incidence_geometry.lean
+++ src/incidence_geometry.lean
@@ -4,40 +4,51 @@ constant Point: Type
 constant Line: Type
 constant lies_on (p: Point) (l: Line) : Prop
 
+local infix ` ~ `:50 := lies_on
+
 /-- 
 For any two distinct points A, B, there exists a unique line containing A, B. 
 -/
-axiom I1: ∀ A B: Point, A ≠ B → ∃ l: Line, 
-  lies_on A l ∧ lies_on B l
-  ∧ (∀ l2:Line, lies_on A l2 ∧ lies_on B l2 →  l = l2)
 
-axiom I1' (A B: Point) (A ≠ B): ∃ l: Line,
-  lies_on A l ∧ lies_on B l 
-  ∧ (∀ l2:Line, lies_on A l2 ∧ lies_on B l2 →  l = l2)
+axiom I1: ∀ A B: Point, A ≠ B → ∃! l: Line, A ~ l ∧ B ~ l
+axiom I1' (A B: Point) (A ≠ B): ∃! l: Line, A ~ l ∧ B ~ l 
+
 
 /--
 Every line contains at least two different points.
 --/
-axiom I2: ∀ l:Line, ∃ A B: Point, 
-  A ≠ B ∧ lies_on A l ∧ lies_on B l
+axiom I2: ∀ l:Line, ∃ A B: Point, A ≠ B ∧ A ~ l ∧ B ~ l
 
 /-- 
 There exist three noncollinear points.
 TODO: Terminar de escribir axioma
 -/
-axiom I3: ∃ A B C: Point, ∃ l m : Line, l ≠ m ∧ lies_on A l 
+axiom I3: ∃ A B C: Point, ∃ l m : Line, l ≠ m ∧ A ~ l 
+
+def common_point (l m: Line) (A: Point) := A ~ l ∧ A ~ m
+def no_common_points (l m: Line) := ∀ A: Point, (A ~ l → ¬ A ~ m) ∧ (A ~ m → ¬ A ~ l)
+
 
 /-- 
 Two distinct lines can have at most one point in common.
 -/
 lemma disctinct_lines_one_common_point: 
   ∀ l m: Line, l ≠ m → (
-    ∃ A: Point, lies_on A l ∧ lies_on A m → ∀ B: Point, lies_on B l → A = B
+    (∃! A: Point, common_point l m A) ∨ (no_common_points l m)
   ) := 
 begin
-  assume l m,
+  intros l m,
   intro h,
-  fapply exists.intro,
+  -- rw exists_unique,
+  have hIl:=I2, 
+  have hIm:=I2, 
+
+  specialize hIl l,
+  specialize hIm m,
+  cases hIl with A hI2',
+  cases hI2' with B hI2'',
+  cases hI2'' with hAB hAlBl,
+  cases hAlBl with hAl hBl,
 
   sorry
 end
