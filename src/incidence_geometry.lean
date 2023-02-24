c6201aed580860efe7d2adeeca7b288e71fe2066
Adrián Lattes <adrianlattes@disroot.org>
2023-02-24 02:11:43 +0000

    Función Line Y Más

--- src/incidence_geometry.lean
+++ src/incidence_geometry.lean
@@ -21,22 +21,48 @@ axiom I2: ∀ l:Line, ∃ A B: Point, A ≠ B ∧ A ~ l ∧ B ~ l
 def collinear (A B C: Point) := ∃ l:Line, A ~ l ∧ B ~ l ∧ C ~ l
 
 /-- 
-There exist three noncollinear points.
+There exist three different noncollinear points.
 -/
-axiom I3: ∃ A B C: Point, ¬ collinear A B C
+axiom I3: ∃ A B C: Point, (A ≠ B ∧ A ≠ C ∧ B≠ C) ∧ ¬ collinear A B C
 
-/-- 
-Three noncollinear points must be different
+/--
+Given two different points get the line that passes through them
 -/
-lemma noncollinear_diff (A B C: Point): 
-  ¬ collinear A B C → A ≠ B ∧ B ≠ C ∧ A ≠ C := 
+noncomputable def line (A B:Point) (h: A ≠ B): 
+  { l: Line // A ~ l ∧ B ~ l ∧ ∀ l' : Line,  A ~ l' ∧ B ~ l' → l' = l } := 
 begin
-  sorry
+  have hAB := I1 A B h,
+  rw exists_unique at hAB,
+  let P := λ l:Line,  A ~ l ∧ B ~ l ∧ ∀ l' : Line,  A ~ l' ∧ B ~ l' → l' = l,
+  have hlP: ∃ l :Line, P l, { tauto },
+  exact classical.indefinite_description P hlP,
 end
 
-def have_common_point (l m: Line) := ∃ A:Point, A ~ l ∧ A ~ m
-def no_common_points (l m: Line) := ¬ have_common_point l m
+/--
+Given a point there exist another two different points different to it
+-/
+lemma exist_ne_points (A: Point) :
+  ∃ B C:Point, A ≠ B ∧ A ≠ C ∧ B ≠ C := 
+begin
+  rcases I3 with ⟨P, Q, R, h⟩,
+  by_cases hAP: A = P,
+  { use Q, use R, rw hAP, exact h.left },
+  { by_cases hAQ: A = Q,
+    { use P, use R, rw hAQ, tauto },
+    { use P, use Q, tauto }},
+end
+
+/--
+For any point there exist one point different to it
+-/
+lemma exist_ne_point (A:Point): ∃ B:Point, A ≠ B := 
+begin
+  rcases exist_ne_points A with ⟨B, -, ⟨hAB, -, -⟩⟩,
+  use B,
+end
+
 def is_common_point (A: Point) (l m: Line) := A ~ l ∧ A ~ m
+def have_common_point (l m: Line) := ∃ A:Point, is_common_point A l m
 
 lemma common_points_and_lines_eq (l m: Line) (A B: Point) : 
   is_common_point A l m ∧ is_common_point B l m ↔ (A ~ l ∧ B ~ l) ∧ (A ~ m ∧ B ~ m) := 
@@ -52,30 +78,39 @@ end
 Two distinct lines can have at most one point in common.
 -/
 lemma disctinct_lines_one_common_point: 
-  ∀ l m: Line, l ≠ m → (
-    (∃! A: Point, is_common_point A l m) ∨ (no_common_points l m)
-  ) := 
+  ∀ l m: Line, l ≠ m → (∃! A: Point, is_common_point A l m) ∨ (¬ have_common_point l m) := 
 begin
   intros l m,
-  contrapose,
-  intro h,
-  push_neg,
-  push_neg at h,
-  cases h with not_unique hlm,
+  contrapose!,
+  rintro ⟨not_unique, hlm⟩,
   rw [exists_unique, push_neg.not_exists_eq] at not_unique,
   push_neg at not_unique,
-  rw [no_common_points, push_neg.not_not_eq] at hlm,
-  cases hlm with A hA,
-  rw ← is_common_point at hA,
-  specialize not_unique A hA,
-  rcases not_unique with ⟨B, ⟨hB, hAB⟩⟩,
+  choose A hA using hlm,
+  rcases not_unique A hA with ⟨B, ⟨hB, hAB⟩⟩,
   rw ne_comm at hAB,
-  have hI1 := I1,
-  specialize hI1 A B hAB,
-  have h: (A ~ l ∧ B ~ l) ∧ (A ~ m ∧ B ~ m),
-  { rw ← common_points_and_lines_eq, 
-    exact ⟨hA, hB⟩ },
-  rw ← unique_of_exists_unique hI1 (and.left h) (and.right h),
+  let hI1 := I1 A B hAB,
+  let hABlm := and.intro hA hB,
+  rw common_points_and_lines_eq at hABlm,
+  rw ← unique_of_exists_unique hI1 hABlm.1 hABlm.2,
+end
+
+/--
+There exist three disctinct lines not through any common point
+-/
+lemma disctinct_lines_not_concurrent:
+  ∃ l m n: Line, l ≠ m ∧ l ≠ n ∧ m ≠ n →
+  ¬ have_common_point l m ∧ ¬ have_common_point l n ∧ ¬ have_common_point m n
+  :=
+begin
+  rcases I3 with ⟨A, B, C, h⟩,
+  rcases h.left with ⟨hAB, hAC, hBC⟩,
+  have AB := line A B hAB,
+  have AC := line A C hAC,
+  have BC := line B C hBC,
+  have hABAC: AB.val ≠ AC.val, { sorry },
+  have hABBC: AB.val ≠ BC.val, { sorry },
+  have hACBC: AC.val ≠ BC.val, { sorry },
+  sorry
 end
 
 /-- 
@@ -83,20 +118,17 @@ For every line there is at least one point not lying on it.
 -/
 lemma line_external_point: ∀ l: Line, ∃ A: Point, ¬ A ~ l :=
 begin
-  have hI3:= I3,
-  rcases hI3 with ⟨A, B, C, h1⟩,
+  rcases I3 with ⟨A, B, C, ⟨-, h1⟩⟩,
   by_contra h2,
   rw collinear at h1,
   push_neg at h1,
   push_neg at h2,
-  cases h2 with l hlA,
-  have hlB := hlA,
-  have hlC := hlA,
-  specialize hlA A,
-  specialize hlB B,
-  specialize hlC C,
-  specialize h1 l hlA hlB,
-  exact h1 hlC
+  cases h2 with l hl,
+  have hAl: A ~ l, { tauto },
+  have hBl: B ~ l, { tauto },
+  have hCl: C ~ l, { tauto },
+  specialize h1 l hAl hBl,
+  exact h1 hCl
 end
 
 /-- 
@@ -104,19 +136,22 @@ For every point there is at least one line not passing through it.
 -/
 lemma point_external_line: ∀ A: Point, ∃ l: Line, ¬ A ~ l :=
 begin
-  sorry 
+  intro A,
+  rcases I3 with ⟨P, Q, R, ⟨⟨hPQ, hPR, hQR⟩, h2⟩⟩,
+  rw collinear at h2,
+  by_cases hAP: A = P,
+  { 
+    let QR := line Q R hQR,
+    use QR.val,
+    rw hAP,
+    rw push_neg.not_exists_eq at h2,
+    specialize h2 QR,
+    rw push_neg.not_and_distrib_eq at h2,
+    rw push_neg.not_and_distrib_eq at h2,
+    sorry
+  },
+  { sorry },
 end
-
-noncomputable 
-def line (A B:Point) (h: A ≠ B) : Line × Prop := begin
-  -- have hAB := I1,
-  -- specialize hAB A B H,
-  -- cases hAB with l hl,
-  -- exact (l,hl)
-  sorry
-end
-
-#check line
 
 /--
 For every point there exist at least two distinct lines that pass through it.
