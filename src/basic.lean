0c0996e350ded383e97006a3e65bebde444e58c7
Adrián Lattes <adrianlattes@disroot.org>
2023-03-01 23:48:43 +0000

    nuevo lema push_neg.non_collinear: reescribir non collinear

--- src/basic.lean
+++ src/basic.lean
@@ -13,6 +13,23 @@ def points_in_line {Point Line : Type*} [has_lies_on Point Line] (A B : Point) (
 def collinear {Point : Type*} (Line: Type*) [has_lies_on Point Line] (A B C : Point) : Prop := 
   ∃ l : Line, A ~ l ∧ B ~ l ∧ C ~ l
 
+namespace push_neg
+lemma non_collinear {Point : Type*} (Line: Type*) [has_lies_on Point Line] (A B C : Point) : 
+  ¬ collinear Line A B C ↔  ∀ x : Line, (¬ A ~ x ∨ ¬ B ~ x ∨ ¬ C ~ x)
+:= begin 
+  rw [collinear, push_neg.not_exists_eq],
+  split,
+  { intros h x, 
+    specialize h x, 
+    rw [push_neg.not_and_distrib_eq, push_neg.not_and_distrib_eq] at h,
+    exact h },
+  { intros h x, 
+    specialize h x, 
+    rw [push_neg.not_and_distrib_eq, push_neg.not_and_distrib_eq],
+    exact h }
+end 
+end push_neg
+
 def different3 {Point : Type*} (A B C : Point) := (A ≠ B ∧ A ≠ C ∧ B ≠ C)
 
 def is_common_point {Point Line : Type*} [has_lies_on Point Line] (A : Point) (l m : Line) := 
