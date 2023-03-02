208098a1303500426697fdb2967a30f91f6d921d
Adrián Lattes <adrianlattes@disroot.org>
2023-03-02 22:38:36 +0000

    Renombrando betweenness_geometry a order_geometry y avanzando en definiciones

---
+++ src/order_geometry.lean
@@ -0,0 +1,30 @@
+import basic
+import incidence_geometry
+
+open has_lies_on
+
+class order_geometry (Point : Type*) (Line : Type*) extends incidence_geometry Point Line :=
+  (between: Point → Point → Point → Prop)
+  (b1 (A B C: Point) : between A B C → different3 A B C ∧ collinear Line A B C ∧ between C B A)
+  (b2 (A B : Point): A ≠ B → ∃ C : Point, between A B C)
+  (b3 (A B C : Point) (h: collinear Line A B C): xor3 (between A B C) (between B A C) (between C A B))
+  -- (b4 (A B C : Point): ¬ collinear Line A B C → )
+
+notation A `*` B `*` C := order_geometry.between A B C
+
+structure seg (Point : Type*) := (A B : Point) (diff : A ≠ B)
+
+structure tri (Point : Type*) := (A B C : Point) (diff : different3 A B C)
+
+instance (Point Line: Type) [bg: order_geometry Point Line]: has_mem Point (seg Point) := 
+  ⟨λ P S, P = S.A ∨ P = S.B ∨ bg.between P S.A S.B⟩
+
+instance (Point Line: Type) [bg: order_geometry Point Line]: has_mem Point (tri Point) := 
+  ⟨λ P T, 
+    P   ∈ (⟨T.A, T.B, T.diff.1⟩   : seg Point) 
+    ∨ P ∈ (⟨T.A, T.C, T.diff.2.1⟩ : seg Point) 
+    ∨ P ∈ (⟨T.B, T.C, T.diff.2.2⟩ : seg Point)⟩
+
+namespace betweenness_geometry
+
+end betweenness_geometry
