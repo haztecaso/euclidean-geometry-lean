a834d93966e89fc9f14a2c59e5f8e315923f92a5
Adrián Lattes <adrianlattes@disroot.org>
2023-03-15 11:52:49 +0000

    linting

--- src/order_geometry.lean
+++ src/order_geometry.lean
@@ -12,16 +12,16 @@ class order_geometry (Point Line : Type*) extends incidence_geometry Point Line 
   (B3 {A B C : Point} (h : collinear Line A B C): xor3 (A * B * C) (B * A * C) (A * C * B))
   (B4 {A B C D : Point} {l : Line} (h_non_collinear : ¬ collinear Line A B C) 
       (hlABC : ¬ (A ~ l ∨ B ~ l ∨ C ~ l)) (hlD : D ~ l) (hADB : A * D * B)
-      : (∃ E ,  E ~ l ∧ (A * E * C)) xor (∃ E, E ~ l ∧ (B * E * C)))
+      : xor (∃ E ,  E ~ l ∧ (A * E * C)) (∃ E, E ~ l ∧ (B * E * C)))
 
 structure Segment (Point : Type*) := (A B : Point) (diff : A ≠ B)
 
-instance (Point Line : Type) [og: order_geometry Point Line]: has_mem Point (Segment Point) := 
+instance (Point Line : Type) [og: order_geometry Point Line] : has_mem Point (Segment Point) := 
   ⟨λ P S, P = S.A ∨ P = S.B ∨ og.between P S.A S.B⟩
 
 structure Triangle (Point : Type*) := (A B C : Point) (diff : different3 A B C)
 
-instance (Point Line : Type) [order_geometry Point Line]: has_mem Point (Triangle Point) := 
+instance (Point Line : Type) [order_geometry Point Line] : has_mem Point (Triangle Point) := 
   ⟨λ P T, 
     P   ∈ (⟨T.A, T.B, T.diff.1⟩   : Segment Point) 
     ∨ P ∈ (⟨T.A, T.C, T.diff.2.1⟩ : Segment Point) 
