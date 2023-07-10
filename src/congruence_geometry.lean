15d8f2a0aec1078f36e060bed35e2c997d9734b4
Adrián Lattes <adrianlattes@disroot.org>
2023-07-10 18:04:04 +0000

    Continuando axiomas de congruencia

--- src/congruence_geometry.lean
+++ src/congruence_geometry.lean
@@ -16,16 +16,19 @@ class congruence_geometry (Point Line : Type*) extends order_geometry Point Line
     (h2 : seg_cong (Segment.mk B C hABC.left.right.right) (Segment.mk E F hDEF.left.right.right)) :
     seg_cong (Segment.mk A C hABC.left.right.left) (Segment.mk D F hDEF.left.right.left)
   )
-  -- (C4 (a : Angle Point Line) (r : Ray Point) (p : Point): ¬ p ~ (line Line a.A a.B) →   ∃! r2 : Ray Point,  sorry )
+  (C4 (α : Angle Point Line) (A B Side : Point) (hAB : A ≠ B) (hSide :  ¬ Side ~ (line Line hAB).val):
+    ∃! C : Point, (plane_same_side' (line Line hAB).val C Side) ∧ (ang_cong α (Angle.mk_from_points C A B sorry)) )
   (C51 (α β γ : Angle Point Line) (hαβ : ang_cong α β) (hαγ : ang_cong α γ) : ang_cong β γ)
   (C52 (α : Angle Point Line) : ang_cong α α)
   (C6 (T1 T2 : Triangle Point Line) 
     (h1: seg_cong (Segment.mk T1.A T1.B T1.diff.left) (Segment.mk T2.A T2.B T2.diff.left))
     (h2: seg_cong (Segment.mk T1.A T1.C T1.diff.right.left) (Segment.mk T2.A T2.C T2.diff.right.left)) 
-    (h3: ang_cong (Angle.mk_from_points T1.B T1.A T1.C _) (_)) 
+    (h3: ang_cong (Angle.mk_from_points T1.B T1.A T1.C T1.non_collinear) (Angle.mk_from_points T2.B T2.A T2.C T2.non_collinear)) 
     : 
+    seg_cong (Segment.mk T1.B T1.C T1.diff.right.right) (Segment.mk T2.B T2.C T2.diff.right.right)
+    ∧ ang_cong (Angle.mk_from_points T1.A T1.B T1.C sorry) (Angle.mk_from_points T2.A T2.B T2.C sorry)
+    ∧ ang_cong (Angle.mk_from_points T1.A T1.C T1.B sorry) (Angle.mk_from_points T2.A T2.C T2.B sorry)
   )
-
 
 
 /- Segment congruence is an equivalence relation -/
